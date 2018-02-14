0cc7e468480864e697ee9e4cd9227b51cd68de3f
Jason Torres <jmtorres@elder.(none)>
2018-02-14 12:22:11 +0000

    updated to compress output file

--- 04.3_append-position-info.R
+++ 04.3_append-position-info.R
@@ -3,7 +3,7 @@ library("dplyr")
 
 "%&%" <- function(a,b) paste0(a,b)
 
-rescomp.dir <- "/well/mccarthy/users/jason/projects/metaxcan_t2d_v2/" 
+rescomp.dir <- "/well/mccarthy/users/jason/projects/metaxcan_t2d_v2/"
 #"/Users/jtorres/FUSE5/projects/metaxcan_t2d_v2/"
 df.dir <- rescomp.dir %&% "data_frames/"
 plot.dir <- rescomp.dir %&% "plots/"
@@ -16,5 +16,8 @@ mod.dir <- rescomp.dir %&% "model_snps/GTEx-V6p-HapMap-2016-09-08/"
 mod.df <- fread("cat " %&% mod.dir %&% "model-snps-v6p.txt")
 snp.df <- fread("cat " %&% mod.dir %&% "snp_keyfile.txt.gz" %&% " | zmore")
 full.df <- inner_join(mod.df,snp.df,by="RSID")
-write.table(x=full.df,file=df.dir%&%"model-snps-v6p_full.txt",
-            sep="\t",quote=FALSE,row.names=FALSE) 
+gz1 <- gzfile(df.dir%&%"model-snps-v6p_full.txt.gz","w")
+#write.table(x=full.df,file=df.dir%&%"model-snps-v6p_full.txt",
+#            sep="\t",quote=FALSE,row.names=FALSE)
+write.table(x=full.df,file=gz1,
+            sep="\t",quote=FALSE,row.names=FALSE)
